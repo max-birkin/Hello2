dcf17f0ea5df323f0d9219156f025ae4760c1861
Birkin Max <86172545+max-birkin@users.noreply.github.com>
2021-07-27 21:46:33 +0300

    Initial Commit

---
+++ Hello2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello2
+//
+//  Created by Birkin on 27.07.2021.
+//  Copyright © 2021 Max Birkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
